b36076cd26b82e99463eb22428e52a24a11fed36
juanmabott <bottaro.juanmanuel@hotmail.com>
2022-06-27 09:28:11 -0300

    subido

--- main.c
+++ main.c
@@ -1,11 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void invierte(char cadena[], char cadinvertida[]);
+void actualizarRecuperados(ePais pais,int recuperadosDia);
 int main()
 {
-    printf("Hello world!\n");
+    char cad[10] = "UTN-FRA";
+    char cadinvertida[10];
+
+    invierte(cad, cadinvertida);
+
+    printf("\nLa cadena invertida es %s. ", cadinvertida);
+
     return 0;
 }
+
+void invierte(char cadena[], char cadinvertida[])
+{
+    int longitud;
+    int j;
+
+    longitud = strlen(cadena);
+    j = longitud-1;
+
+    for (int i=0;i<longitud;i++)
+    {
+        cadinvertida[i] = cadena[j];
+        j--;
+    }
+    cadinvertida[longitud]='\0';
+    return;
+}
+
 
 
 typedef struct
@@ -21,7 +47,6 @@ void actualizarRecuperados(ePais pais,int recuperadosDia)
 {
     pais.recuperados+=recuperadosDia;
 }
-
 
 int ordenarCaracteres(char vec[], int tam)
 {
